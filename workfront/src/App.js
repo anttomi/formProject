1e9897cb0a688fdb5b493f356f879a7703e46e86
anttomi <tomi.salo@myy.haaga-helia.fi>
2021-04-08 16:30:42 +0300

    survey demo

--- workfront/src/App.js
+++ workfront/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import SurveyComponent from './SurveyComponent';
+import "survey-react/survey.css";
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Ohjelmistoprojekti demo</h1>
+      <SurveyComponent />
     </div>
   );
 }
