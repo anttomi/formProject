7cbfe506200a49ec771bce96d37c306ebb9ceafc
anttomi <tomi.salo@myy.haaga-helia.fi>
2021-04-20 13:33:08 +0300

    Rest service

--- src/main/java/form/project/work/web/FormController.java
+++ src/main/java/form/project/work/web/FormController.java
@@ -1,11 +1,16 @@
 package form.project.work.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import form.project.work.domain.Question;
 import form.project.work.domain.QuestionRepository;
 
 
@@ -13,17 +18,24 @@ import form.project.work.domain.QuestionRepository;
 public class FormController {
 	
 	@Autowired
-	private QuestionRepository repository;
+	private QuestionRepository qRepository;
 	
 	@RequestMapping(value="/login")
-    public String login() {	
-        return "login";
-    }
+	public String login() {	
+	    return "login";
+	}
 	
-	@RequestMapping(value = "/questions", method = RequestMethod.GET)
+	@RequestMapping(value = "/questionlist", method = RequestMethod.GET)
 	public String Questions(Model model) {
-		model.addAttribute("questions", repository.findAll());
+		model.addAttribute("questions", qRepository.findAll());
 		return "questions";
 	}
+	
+	@GetMapping(value = "/questions")
+	public @ResponseBody List<Question> questionRest() {
+	    return (List<Question>) qRepository.findAll();
+	}
+	
+	
 
 }
