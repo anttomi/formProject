916e0441e9099c0075931767817d2a0cc7d51c21
Nekmayne <bgm035@myy.haaga-helia.fi>
2021-04-22 10:07:54 +0300

    front end updates

--- workfront/src/Form.js
+++ workfront/src/Form.js
@@ -6,7 +6,9 @@ import "survey-react/survey.css";
 function Form() {
 
     const [question, setQuestion] = useState({title: "", answer: "", type: ""});
-    
+    const [answer, setAnswer] = useState()
+    const [complete, setComplete] = useState(false)
+
     const fetchQuestion = () => {
         fetch('https://fromproject.herokuapp.com/questions')
         .then(response => response.json())
@@ -14,8 +16,41 @@ function Form() {
         .catch(err => console.error(err))
    } 
 
+    const onComplete = () => {
+        setComplete (true);
+      }
+
+   let json = {
+    questions: [
+      {
+        type: question.type,
+        title: question.title,
+        answer: question.answer,
+        choices: [
+          "Erittäin huonoo",
+          "Hyvveee",
+          "Huonoo",
+          "Vaihtelevaa",
+        ]
+    }
+    ]
+    }
+
+    var surveyRender = !complete ? (
+        <Survey.Survey
+          json={json}
+          onComplete={onComplete}
+    
+        />
+    
+    ) : null
+    var completed = complete ? (
+        <div>Vastasit kaikkiin pakollisiin kysymyksiin. Kiitos</div>
+      ) : null;
    return(
        <div>
+        {surveyRender}
+        {completed}
        <button onClick={fetchQuestion}>LOL</button>
        <p>{question.title}</p>
        </div>
