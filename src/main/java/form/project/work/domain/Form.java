6c1072c8ebff311ed203bd60dc53ee9601d20ea0
anttomi <tomi.salo@myy.haaga-helia.fi>
2021-04-23 14:22:47 +0300

    form entity

--- src/main/java/form/project/work/domain/Form.java
+++ src/main/java/form/project/work/domain/Form.java
@@ -3,6 +3,7 @@ package form.project.work.domain;
 import java.util.List;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -10,6 +11,7 @@ import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+@Entity
 public class Form {
     
     @Id
