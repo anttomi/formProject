90ba3d498dfb215cedaea966969589d498752e95
anttomi <tomi.salo@myy.haaga-helia.fi>
2021-04-26 15:05:55 +0300

    front handling

--- workfront/src/Form.js
+++ workfront/src/Form.js
@@ -1,53 +1,61 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import * as Survey from "survey-react";
 import "survey-react/survey.css";
 
 
 function Form() {
+  
+  const [question, setQuestion] = useState({id: null, title: "", type: "", choices: ([]) });
+  const [answer, setAnswer] = useState()
+  
+  const [complete, setComplete] = useState(false)
+ 
 
-    const [question, setQuestion] = useState({title: "", type: "", choices: ([])});
-    const [answer, setAnswer] = useState()
-    const [complete, setComplete] = useState(false)
+  useEffect (() => {
+
+    fetchQuestion();
+    },[])
 
 
-    const fetchQuestion = () => {
-      fetch('https://formproject6.herokuapp.com/questions')
+  const fetchQuestion = () => {
+    fetch('https://formproject6.herokuapp.com/questions')
       .then(response => response.json())
-      .then(data => { json += "questions: ["
-        json += data
-        json += "]"
-        
-  },
-  console.log(json));
+      .then(data => setQuestion({id: data[0].id, title: data[0].title, type: data[0].type, choices: data[0].choices}))
+      .catch(err => console.error(err))
+  }
 
-      }
-    
   let json = {
-    
+    questions: [
+      {
+        id: question.id,
+        type: question.type,
+        title: question.title,
+        choices: question.choices
+      }
+     ]
     }
 
-    const onComplete = () => {
-        setComplete (true);
-      }
+  const onComplete = () => {
+    setComplete(true);
+  }
 
-    var surveyRender = !complete ? (
-        <Survey.Survey
-          json={json}
-          onComplete={onComplete}
-    
-        />
-    
-    ) : null
-    var completed = complete ? (
-        <div>Vastasit kaikkiin pakollisiin kysymyksiin. Kiitos</div>
-      ) : null;
-   return(
-       <div>
-        {surveyRender}
-        {completed}
-       <button onClick={fetchQuestion}>LOL</button>
-       </div>
-   );
+  var surveyRender = !complete ? (
+    <Survey.Survey
+      json={json}
+      onComplete={onComplete}
+
+    />
+
+  ) : null
+  var completed = complete ? (
+    <div>Vastasit kaikkiin pakollisiin kysymyksiin. Kiitos</div>
+  ) : null;
+  return (
+    <div>
+      {surveyRender}
+      {completed}
+    </div>
+  );
 }
 
 export default Form;
